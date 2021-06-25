39639309131a90f194289acffd9438f64d4dc093
jonlazarini <jlazarini@deloitte.co.uk>
2021-06-25 12:32:21 +0100

    fix(feat-19017): more attempt to close int test

--- tests/scripts/tearDown.ts
+++ tests/scripts/tearDown.ts
@@ -1,18 +1,32 @@
 /* @ts-ignore */
 module.exports = async () => {
-  console.log(`
+  return new Promise((resolve, reject) => {
+    console.log(`
       Integration tests ✅
       ...Tearing down Integrations tests...
     `);
-  // @ts-ignore
-  const { pid } = global.__SERVER__;
-  // @ts-ignore
-  global.__SERVER__.on("close", () => {
+
+    console.log("global.__SERVER__");
+    //@ts-ignore
+    console.log(global.__SERVER__);
+    console.log("process");
+    console.log(process);
+
     // @ts-ignore
-    console.log(`Killed pid: ${pid}...
+    const { pid } = global.__SERVER__;
+    console.log("pid");
+    console.log(pid);
+    // @ts-ignore
+    global.__SERVER__.close((err) => {
+      console.log(err);
+      if (err) return reject(err);
+      // @ts-ignore
+      console.log(`Killed pid: ${pid}...
     `);
-    // process.exit(0);
-    Promise.resolve("✔");
+      resolve("✔");
+      // process.exit(0);
+    });
+    console.log("after close");
+    // process.kill(pid);
   });
-  process.kill(pid);
 };
