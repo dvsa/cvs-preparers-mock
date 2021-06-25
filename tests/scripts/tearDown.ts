9d58f5e40309318634487597e0ece5257327eb60
jonlazarini <jlazarini@deloitte.co.uk>
2021-06-25 13:06:02 +0100

    fix(feat-19017): more attempt

--- tests/scripts/tearDown.ts
+++ tests/scripts/tearDown.ts
@@ -9,29 +9,30 @@ module.exports = async () => {
     // console.log("global.__SERVER__");
     // @ts-ignore
     // console.log(global.__SERVER__.kill);
-    console.log("process");
-    console.log(process);
+    // console.log("process");
+    // console.log(process);
 
     // @ts-ignore
     const { pid } = global.__SERVER__;
     console.log("send kill for pid");
     console.log(pid);
 
-    
     // @ts-ignore
     global.__SERVER__.on("exit", (code) => {
       console.log(`process terminated with code ${code}`);
-      resolve('ok');
+      process.exit(0);
+      resolve("ok");
     });
 
     try {
-    // @ts-ignore
+      // @ts-ignore
       global.__SERVER__.kill();
-    } catch(e) {
-      console.error(`Couldn't kill process ${e}`)
-      reject(e)
+      console.log("carrying");
+    } catch (e) {
+      console.error(`Couldn't kill process ${e}`);
+      reject(e);
     }
-    
+
     // global.__SERVER__.close((err) => {
     //   console.log(err);
     //   if (err) {
@@ -43,7 +44,7 @@ module.exports = async () => {
     //   resolve("✔");
     //   // process.exit(0);
     // });
-    
+
     // process.kill(pid);
   });
 };
